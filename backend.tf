1263899797d3eb3aba01c3990a4e4a0e5eb5b9fe
abettie <abechinoid@gmail.com>
2026-03-02 17:35:07 +0900

    状態管理用S3バケット名にuuid追加＆deprecated設定修正

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
+    bucket         = "terraform-state-da281549-5c33-41b0-9677-4d5ebe2b2e95"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
+    use_lockfile   = true
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"
   }
 }
