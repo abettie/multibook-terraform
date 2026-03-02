4f4bb8d344691c07c1f21ead742a18d464f0f05e
abettie <abechinoid@gmail.com>
2026-03-02 21:20:01 +0900

    デフォルトタグ追加

--- provider.tf
+++ provider.tf
@@ -1,9 +1,21 @@
 provider "aws" {
   alias  = "tokyo"
   region = var.aws_region_tokyo
+
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "virginia"
   region = var.aws_region_virginia
+
+  default_tags {
+    tags = {
+      ManagedBy = "Terraform"
+    }
+  }
 }
